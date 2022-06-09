450ea57e007f3fd412082f5baecad4e4d0e7c9f1
Austin Gilbert <AGilbert@rbaconsulting.com>
2022-06-09 12:55:16 -0500

    Adding an javascript api and controller to match the service

--- uPersonalize/Controllers/PersonalizationController.cs
+++ uPersonalize/Controllers/PersonalizationController.cs
@@ -2,6 +2,8 @@
 using uPersonalize.Interfaces;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Cms.Web.Common.Controllers;
+using System.Threading.Tasks;
+using uPersonalize.Models;
 
 namespace uPersonalize.Controllers
 {
@@ -18,16 +20,30 @@ namespace uPersonalize.Controllers
 			_personalizationService = personalizationService;
 		}
 
+		[HttpPost]
+		[Route("umbraco/uPersonalize/Personalization/TrackUser/{pageId}")]
+		public async Task TrackUser(int pageId)
+		{
+			await _personalizationService.TrackUser(pageId);
+		}
+
+		[HttpPost]
+		[Route("umbraco/uPersonalize/Personalization/IsMatch")]
+		public async Task<bool> IsMatch([FromBody] PersonalizationFilter filter)
+		{
+			return await _personalizationService.IsMatch(filter);
+		}
+
 		/// <summary>
 		/// ~/Umbraco/uPersonalize/Personalization/PageVisit
 		/// </summary>
 		[HttpPost]
 		[Route("umbraco/uPersonalize/Personalization/TriggerEvent/{eventName}")]
-		public void TriggerEvent(string eventName)
+		public async Task TriggerEvent(string eventName)
 		{
 			if (!string.IsNullOrWhiteSpace(eventName))
 			{
-				_personalizationService.TryTriggerEvent(eventName);
+				await _personalizationService.TryTriggerEvent(eventName);
 			}
 		}
 
@@ -36,12 +52,32 @@ namespace uPersonalize.Controllers
 		/// </summary>
 		[HttpPost]
 		[Route("umbraco/uPersonalize/Personalization/PageVisit/{pageId}")]
-		public void PageVisit(string pageId)
+		public async Task PageVisit(string pageId)
 		{
 			if (!string.IsNullOrWhiteSpace(pageId))
 			{
-				_personalizationService.TryPageVisit(pageId);
+				await _personalizationService.TryPageVisit(pageId);
 			}
+		}
+
+		/// <summary>
+		/// ~/Umbraco/uPersonalize/Personalization/PageVisit
+		/// </summary>
+		[HttpGet]
+		[Route("umbraco/uPersonalize/Personalization/GetTriggeredEventCount/{eventName}")]
+		public async Task<int> GetTriggeredEventCount(string eventName)
+		{
+			return await _personalizationService.GetTriggeredEventCount(eventName);
+		}
+
+		/// <summary>
+		/// ~/Umbraco/uPersonalize/Personalization/PageVisit
+		/// </summary>
+		[HttpGet]
+		[Route("umbraco/uPersonalize/Personalization/GetPageVisitCount/{pageId}")]
+		public async Task<int> GetPageVisitCount(string pageId)
+		{
+			return await _personalizationService.GetPageVisitCount(pageId);
 		}
 	}
 }
