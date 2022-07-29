9c34a2e300f5bd7c441bc4544764cd16a88b164f
Austin Gilbert <AGilbert@rbaconsulting.com>
2022-07-29 15:58:40 -0500

    Migration hotfixes

--- uPersonalize/Migrations/uPersonalizeDatabaseMigration.cs
+++ uPersonalize/Migrations/uPersonalizeDatabaseMigration.cs
@@ -8,6 +8,9 @@ using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
 using uPersonalize.Migrations.Plans;
 using System.Reflection;
+using System;
+using Microsoft.Extensions.Logging;
+using System.Numerics;
 
 namespace uPersonalize.Migrations
 {
@@ -16,17 +19,20 @@ namespace uPersonalize.Migrations
 		private readonly string _uPersonalizeSettingsKey = "uPersonalize_Settings";
 		private readonly string _uPersonalizeReportingKey = "uPersonalize_Reporting";
 
+		private readonly ILogger<uPersonalizeDatabaseMigration> _logger;
 		private readonly IScopeProvider _scopeProvider;
 		private readonly IMigrationPlanExecutor _migrationPlanExecutor;
 		private readonly IKeyValueService _keyValueService;
 		private readonly IRuntimeState _runtimeState;
 
-		public uPersonalizeDatabaseMigration(
+
+		public uPersonalizeDatabaseMigration(ILogger<uPersonalizeDatabaseMigration> logger,
 			IScopeProvider scopeProvider,
 			IMigrationPlanExecutor migrationPlanExecutor,
 			IKeyValueService keyValueService,
 			IRuntimeState runtimeState)
 		{
+			_logger	= logger;
 			_scopeProvider = scopeProvider;
 			_migrationPlanExecutor = migrationPlanExecutor;
 			_keyValueService = keyValueService;
@@ -40,29 +46,39 @@ namespace uPersonalize.Migrations
 				return;
 			}
 
-			var currentVersion = _keyValueService.GetValue($"Umbraco.Core.Upgrader.State+{_uPersonalizeSettingsKey}");
-			var newVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			var currentVersion = string.Empty;
 
-			var settingsPlan = new MigrationPlan(_uPersonalizeSettingsKey);
-			var reportingPlan = new MigrationPlan(_uPersonalizeReportingKey);
-
-			if(!newVersion.Equals(currentVersion) && !newVersion.Equals(currentVersion))
+			try
 			{
-				settingsPlan.From(currentVersion).To<uPersonalizeSettingsTable>(newVersion);
-				reportingPlan.From(currentVersion).To<uPersonalizeReportingTable>(newVersion);
+				currentVersion = _keyValueService.GetValue($"Umbraco.Core.Upgrader.State+{_uPersonalizeSettingsKey}");		
+			}
+			catch(Exception e)
+			{
+				_logger.LogWarning(e.Message);
+			}
+			finally
+			{
+				var newVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+				var settingsPlan = new MigrationPlan(_uPersonalizeSettingsKey);
+				var reportingPlan = new MigrationPlan(_uPersonalizeReportingKey);
+
+				if (!string.IsNullOrWhiteSpace(currentVersion) && !newVersion.Equals(currentVersion))
+				{
+					settingsPlan.From(currentVersion).To<uPersonalizeSettingsTable>(newVersion);
+					reportingPlan.From(currentVersion).To<uPersonalizeReportingTable>(newVersion);
+				}
+
+				settingsPlan.From(string.Empty).To<uPersonalizeSettingsTable>(newVersion);
+				reportingPlan.From(string.Empty).To<uPersonalizeReportingTable>(newVersion);
+
+				var upgrader = new Upgrader(settingsPlan);
+				upgrader.Execute(_migrationPlanExecutor, _scopeProvider, _keyValueService);
+
+				upgrader = new Upgrader(reportingPlan);
+				upgrader.Execute(_migrationPlanExecutor, _scopeProvider, _keyValueService);
 			}
 
-			settingsPlan.From(string.Empty).To<uPersonalizeSettingsTable>(newVersion);
-			reportingPlan.From(string.Empty).To<uPersonalizeReportingTable>(newVersion);
-
-			ExecutePlan(settingsPlan);
-			ExecutePlan(reportingPlan);
-		}
-
-		private void ExecutePlan(MigrationPlan plan)
-		{
-			var upgrader = new Upgrader(plan);
-			upgrader.Execute(_migrationPlanExecutor, _scopeProvider, _keyValueService);
 		}
 	}
 }
