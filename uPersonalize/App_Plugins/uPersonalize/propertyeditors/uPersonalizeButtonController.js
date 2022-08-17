ed96165d933414a7283cf488e597225e18e997df
Austin Gilbert <AGilbert@rbaconsulting.com>
2022-08-17 10:20:04 -0500

    Enhancing the personalization button property editor and adding a remove personalization button.

--- uPersonalize/App_Plugins/uPersonalize/propertyeditors/uPersonalizeButtonController.js
+++ uPersonalize/App_Plugins/uPersonalize/propertyeditors/uPersonalizeButtonController.js
@@ -1,4 +1,40 @@
 ﻿angular.module("umbraco").controller("uPersonalizeButtonController", function ($scope, editorService) {
+	$scope.editGridItemSettings = function () {
+		var dialogOptions = {
+			title: "uPersonalize",
+			view: "/App_Plugins/uPersonalize/dialogs/uPersonalizeDialog.html",
+			size: "small",
+			uPersonalizeConfig: config,
+			submit: function (model) {
+				$scope.model.value = JSON.stringify(model);
+				$scope.setView();
+				editorService.close();
+			},
+			close: function () {
+				editorService.close();
+			}
+		};
+
+		editorService.open(dialogOptions);
+	};
+
+	$scope.removePersonalization = function () {
+		$scope.model.value = null;
+		$scope.setView();
+	};
+
+	$scope.setView = function () {
+		if ($scope.model.value) {
+			vm.buttonText = 'Edit Personalization';
+			vm.showDeleteButton = true;
+		}
+		else {
+			vm.buttonText = 'Personalize';
+			vm.showDeleteButton = false;
+		}
+	};
+
+	var vm = this;
 	var config = $scope.model.value ? JSON.parse($scope.model.value) : {
 		condition: '',
 		action: '',
@@ -11,28 +47,11 @@
 		additionalClasses: ''
 	};
 
+	$scope.setView();
+
 	if (config) {
 		if (config.dateTimeCompare) {
 			config.dateTimeCompare = config.dateTimeCompare.replace('Z', '');
 		}
-
-		$scope.editGridItemSettings = function () {
-			var dialogOptions = {
-				title: "uPersonalize",
-				view: "/App_Plugins/uPersonalize/dialogs/uPersonalizeDialog.html",
-				size: "small",
-				uPersonalizeConfig: config,
-				submit: function (model) {
-					$scope.model.value = JSON.stringify(model);
-
-					editorService.close();
-				},
-				close: function () {
-					editorService.close();
-				}
-			};
-
-			editorService.open(dialogOptions);
-		};
 	}
 });
